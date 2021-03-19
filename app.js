2356fa6d9bfad0d9c45f35df774e9798e4f5b8c5
MuratTuz <murattuzlali@gmail.com>
2021-03-19 11:01:32 +0100

    Second commit

--- app.js
+++ app.js
@@ -7,16 +7,17 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-
-
 const filePath = './OrnekDosya.txt';
+
 fs.readFile(filePath, (err, data) => {
     if (err) {
         console.log('There is error reading the file' + err.message);
 
-        rl.question('New file will be created, so please enter a name without extension for the new file :  ', (fileName) => {
-            fs.appendFile('./' + fileName + '.txt', 'A new file is created', 'utf8', (err) => {
-                if (err) console.log('Error in file creation with the error message is : ' + err.message);
+        rl.question('New file will be created, so please enter a name without extension for the new file :  ', (name) => {
+            const fileName = name + '.txt';
+            fs.appendFile(fileName, 'A new file is created', 'utf8', (err) => {
+                if (err) console.log('Error in file creation with the error message is : ' + err.message)
+                else console.log(`File ${fileName} is created succesfully`);
             });
 
             rl.close();
